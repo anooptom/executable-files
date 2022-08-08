bc14dfae647cfe85388339e63c096f4fb42e5d7b
Anoop <anoopkuzhivayalil@gmail.com>
2022-08-08 17:21:29 +0530

    sends automated message to specific recipients

---
+++ send hello.bat
@@ -0,0 +1,13 @@
+adb shell input tap 933 1617
+timeout 1
+adb shell input tap 699 1271
+timeout 1
+adb shell input tap 898 205
+timeout 1
+adb shell input text "saved"
+timeout 1
+adb shell input tap 900 368
+timeout 1
+adb shell input text "Hello, Hi there!"
+timeout 1
+adb shell input tap 982 1485
