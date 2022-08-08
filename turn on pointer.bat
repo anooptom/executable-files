8cc8c269eedf39ea0f42b5b301ae07cafb601ab3
Anoop <anoopkuzhivayalil@gmail.com>
2022-08-08 17:17:42 +0530

    Turn on pointer location in phone

---
+++ turn on pointer.bat
@@ -0,0 +1,9 @@
+adb shell input tap 738 1938
+timeout 1
+adb shell input tap 500 690
+timeout 2
+adb shell input text "Pointer"
+timeout 1
+adb shell input tap 844 782
+timeout 2
+adb shell input tap 962 2229
